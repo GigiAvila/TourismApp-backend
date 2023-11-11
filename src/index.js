39a3fd03b9d29002204a99e849a7c5ad6d340ca7
GigiAvila <gigi.s.avila@gmail.com>
2023-11-11 12:50:58 +0100

    changeInIndex

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-require('dotenv').config()
 const express = require('express')
 const app = express()
 require('./Config/db')
@@ -9,7 +8,7 @@ const { setError } = require('./config/error')
 const seedFunctions = require('./Config/seed')
 seedFunctions()
 
-//CLOUDINARY
+// CLOUDINARY
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.API_KEY,
@@ -38,7 +37,15 @@ app.use(limiter)
 app.use(express.json({ limit: '1mb' }))
 app.use(express.urlencoded({ limit: '1mb', extended: true }))
 
-//Router
+// HEADERS
+app.use((_req, res, next) => {
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  res.header('Access-Control-Allow-Headers', 'Content-Type')
+  next()
+})
+app.disable('x-powered-by')
+
+// Router
 const mainRouter = require('./Routes/indexRouter')
 app.use('/api', mainRouter)
 
@@ -60,10 +67,3 @@ app.listen(PORT, () => {
     `La aplicación está corriendo en el puerto http://localhost:${PORT}`
   )
 })
-
-app.use((_req, res, next) => {
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
-  res.header('Access-Control-Allow-Headers', 'Content-Type')
-  next()
-})
-app.disable('x-powered-by')
