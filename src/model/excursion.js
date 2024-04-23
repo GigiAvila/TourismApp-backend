f30fc8055f40c00e1030daaf82451cea35205fee
GigiAvila <gigi.s.avila@gmail.com>
2024-04-23 13:50:06 +0200

    Mensaje de confirmación

--- src/model/excursion.js
+++ src/model/excursion.js
@@ -5,6 +5,8 @@ const excursionSchema = new mongoose.Schema(
   {
     name: { type: String, required: true, trim: true },
     excursionImg: { type: String, required: false, trim: true },
+    duration: { type: Number, required: true },
+    description: { type: String, required: true },
     users: [
       { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: false }
     ]
